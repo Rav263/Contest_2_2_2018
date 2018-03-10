5de7fc9708d865fcedd2ff9c1024fdb22521407a
RAV263 <nickiforov.nik@gmail.com>
2018-03-10 09:27:44 +0300

    Разобраны условные операторы

--- A.asm
+++ A.asm
@@ -11,11 +11,34 @@ CMAIN:
 
   MOV ECX, DWORD[n]
 
+  GET_DEC 4, EBX
+  DEC ECX
+  GET_DEC 4, EDX
+  DEC ECX
+  ;GET_DEC 4, ESI
+  ;DEC_ECX
+
+
   top:
     GET_DEC 4, EAX
 
-
+    CMP EBX, EAX
+    jge a 
+      MOV EDX, EBX
+      MOV EBX, EAX
+    jmp b
+    a:
+      cmp EDX, EAX
+      jge b
+        MOV EDX, EAX
+    b:
   loop top
+
+  PRINT_DEC 4, EBX
+  NEWLINE
+
+  PRINT_DEC 4, EDX
+  NEWLINE
 
   MOV EAX, 0
   RET
