6cc52d64ab68eba2079e9106d25ebfdca42b1567
RAV263 <nickiforov.nik@gmail.com>
2018-03-12 12:43:20 +0300

    решена задача C

--- C.asm
+++ C.asm
@@ -7,12 +7,14 @@ section .bss
   n resd 1
   m resd 1
   k resd 1
+  now resd 1
+
 
 section .data
 global CMAIN
 CMAIN:
-  GET_DEC 4, [n]
   GET_DEC 4, [m]
+  GET_DEC 4, [n]
   GET_DEC 4, [k]
 
   MOV ECX, DWORD[n]
@@ -21,22 +23,64 @@ CMAIN:
   MOV EAX, 0
 
   l1:
-    GET_DEC 4, [a + EAX]
+    MOV EBX, EAX
+    IMUL EBX, 4
+    GET_DEC 4, [a + EBX]
     ADD EAX, 1
   loop l1
 
-  MOV ECX, DWORD[m]
+  MOV ECX, DWORD[n]
   IMUL ECX, DWORD[k]
 
   MOV EAX, 0
 
   l2:
-    GET_DEC 4, [b + EAX]
+    MOV EBX, EAX
+    IMUL EBX, 4
+    GET_DEC 4, [b + EBX]
     ADD EAX, 1
   loop l2
 
+  MOV EAX, 0
 
+  top1:
+    MOV EBX, 0
+    top2:
+      MOV ESI, 0
+      MOV ECX, 0
+      top3:
+        MOV EDI, EAX
+        IMUL EDI, DWORD[n]
+        ADD EDI, ECX
+        IMUL EDI, 4
+        MOV EDX, DWORD[a + EDI]
 
+        MOV EDI, ECX
+        IMUL EDI, DWORD[k]
+        ADD EDI, EBX
+        IMUL EDI, 4
+        IMUL EDX, DWORD[b + EDI]
+
+        ADD ESI, EDX
+
+        ADD ECX, 1
+        cmp ECX, DWORD[n]
+      jnz top3
+      MOV EDI, EAX
+      IMUL EDI, DWORD[k]
+      ADD EDI, EBX
+      IMUL EDI, 4
+      MOV DWORD[c + EDI], ESI
+      
+      PRINT_DEC 4, ESI
+      PRINT_STRING " "
+      ADD EBX, 1
+      cmp EBX, DWORD[k]
+    jnz top2
+    NEWLINE
+    ADD EAX, 1
+    cmp EAX, DWORD[m]
+  jnz top1
 
   MOV EAX, 0
   RET
